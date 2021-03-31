e5a112c003c57a7bb7d13a94ecbcd68769869943
Oliver Booth <me@olivr.me>
2021-03-31 23:09:55 +0100

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private CharacterController2D characterController = null; // We set it to null by default to avoid warnings
+    [SerializeField] private Animator animator = null;
+
+    // This allows you to get input values from this script just by typing "... = playerController.MoveInput"
+    public float MoveInput { get; private set; } // This variable can be read from anywhere but only this script can SET it
+    public bool Jump { get; private set; }
+    public bool Crouch { get; private set; }
+
+    public bool waitsForJump;
+    public bool wasJumpLastTime;
+
+    private void Update()
+    {
+        characterController.Move(MoveInput, Crouch, waitsForJump);
+        waitsForJump = false; // We have to reset jump so our player won't jump all the time
+    }
+
+
+    // Those methods are called by Player Input on the same game object
+    public void OnMove(InputValue value)
+    {
+        MoveInput = value.Get<float>(); // Gets movement value in -1..1 scale
+    }
+
+    public void OnJump(InputValue value)
+    {
+        Jump = value.isPressed; // If Jump is pressed, we set jump to true
+
+        // If we pressed jump, we wait tell our class to jump as soon as possible
+        if (!wasJumpLastTime && Jump)
+            waitsForJump = true;
+
+        wasJumpLastTime = Jump;
+    }
+
+    public void OnCrouch(InputValue value)
+    {
+        Crouch = value.isPressed; // We check if crouch is held down or not
+    }
+
+
+    // Used for animations
+    public void SetCrouchOnAnimator(bool state)
+    {
+        animator.SetBool("Crouch", state);
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# Challenge #1 (23 May 2020)
+## Recreate Super Mario Pipes
+---
+
+Here's a quick breakdown of some steps involved:
+
+ - Switch scenes when player interacts with a pipe
+ - Spawn player in front of the proper exit pipe
+ - Add animation for entering and exiting the pipe
+ - Add fade out and expanding circle when switching scene like in new Mario Maker 2
+ - Bonus: Add coin collecting with counter persisting through scenes
+ - Bonus 2: Only spawn coins if they haven't already been collected
+ - Artistic Bonus: Make it prettier; add animations, moving clouds and so on
+
+This starter project includes two scenes, as well as a character controller (https://www.youtube.com/watch?v=dwcT-Dch0bA). The project was made in Unity 2019.3.13f - opening the project in any other version may break stuff (though anything 2019.3 should be fine)! Credit to Kenney (https://kenney.nl/assets) for the great assets!
